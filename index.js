628699beb0961b0aee505367100325757a7770fc
Lars Orbegozo <wezenkerrash44@gmail.com>
2023-09-22 10:19:07 -0300

    user-inputted colorable grid implemented

--- index.js
+++ index.js
@@ -1,8 +1,51 @@
 const gridContainer = document.querySelector('#container')
+const button = document.querySelector('button')
+let htmlElements = ""
 
-for(let i = 1; i <= 256; i++) {
-    let gridDiv = document.createElement('div')
-    gridDiv.classList.add('grid')
-    gridDiv.textContent = `${i}`
-    gridContainer.appendChild(gridDiv)
+createGrid(16)
+
+// Paint the grid
+const allGridDiv = document.querySelectorAll('.grid')
+allGridDiv.forEach((individualDiv) => {
+    individualDiv.addEventListener('mouseenter', () => {
+        individualDiv.style.backgroundColor = 'blue'
+    })
+})
+
+button.addEventListener('click', () => {
+    resetGrid()
+    
+    let userChoice = parseInt(prompt("Please enter a number of squares per side:\n(max: 50)", ""))
+
+    if(isNaN(userChoice) || userChoice > 50 || userChoice < 0) {
+        alert("Enter a valid number.")
+        resetGrid()
+        createGrid(16)
+    } else {
+        resetGrid()
+        createGrid(userChoice)
+    }
+    
+    // Paint the grid
+    const allGridDiv = document.querySelectorAll('.grid')
+    allGridDiv.forEach((individualDiv) => {
+    individualDiv.addEventListener('mouseenter', () => {
+        individualDiv.style.backgroundColor = 'blue'
+        })
+    })
+})
+
+function createGrid(x) {
+    for(let rows = 1; rows <= x; rows++) {
+        for(let column = 1; column <= x; column++) {
+            let size = 960/x
+            htmlElements += `<div class="grid" style="width: ${size-2}px; height:${size-2}px;"></div>`
+            gridContainer.innerHTML = htmlElements
+        }
+    }
+}
+
+function resetGrid() {
+    htmlElements = ""
+    gridContainer.innerHTML = htmlElements
 }
